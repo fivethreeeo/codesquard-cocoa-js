f542c7bcd1adabc281b0fe4d24290ac03a951781
Sangbeom Heo <mansaout@gmail.com>
2021-11-02 20:54:10 +0900

    추가미션 구현 완료

--- mission_01_getArea.js
+++ mission_01_getArea.js
@@ -33,9 +33,99 @@
   - 기능 별로 함수 최대한 쪼개기
   - 함수 선언으로만 작성
   - getArea() 호출 시 도형 오기입 안내 메시지
+
 */
 
 'use-strict';
+
+// 전역변수
+let logArr = new Array();
+
+// getArea()
+// 1. shape 별로 넓이 값(area)을 구하는 함수를 호출
+// 2. 함수호출로그(shape, area)를 로그배열(logArr)에 저장
+function getArea(shape, ...size) {
+  let area;
+
+  switch (shape) {
+    case 'rect':
+      area = getRect(...size);
+      break;
+    case 'trapezoid':
+      area = getTrapezoid(...size);
+      break;
+    case 'circle':
+      area = Math.round(getCircle(...size));
+      break;
+    default:
+      console.log('도형을 정확하게 입력해주세요.');
+      return false;
+  }
+  saveExecutionSequence(shape, area);
+}
+
+// getRect()
+// 1. 넓이 값 계산 -> 리턴
+function getRect(width, height) {
+  return width * height;
+}
+
+// Trapezoid()
+// 1. 넓이 값 계산 -> 리턴
+function getTrapezoid(shortBase, longBase, height) {
+  shortBase = parseInt(shortBase);
+  longBase = parseInt(longBase);
+  height = parseInt(height);
+  return ((shortBase + longBase) * height) / 2;
+}
+
+// getCircle()
+// 1. if radius2의 값이 있으면 sumCircles() 리턴
+// 2. 아니면 넓이 값 계산 -> 리턴
+function getCircle(radius1, radius2) {
+  radius1 = parseInt(radius1);
+  radius2 = parseInt(radius2);
+  const PI = Math.PI;
+  if (radius2 !== undefined) {
+    return sumCircles(radius1, radius2);
+  }
+  return radius1 * radius1 * PI;
+}
+
+// sumCircles()
+// 1. 반지름이 radius1~radius2인 원 넓이의 합 계산 -> 리턴
+function sumCircles(radius1, radius2) {
+  const PI = Math.PI;
+  let sum = 0;
+  for (let i = radius1; i < radius2 + 1; i++) {
+    sum += i * i * PI;
+  }
+  return sum;
+}
+
+// saveExecutionSequence()
+// 1. 배열 logArr에 shape, area값 문자열 저장
+function saveExecutionSequence(shape, area) {
+  logArr.push(`${shape}, ${area}`);
+}
+
+// printExecutionSequence()
+// 1. 배열 logArr를 문자열로 변환 -> 출력
+function printExecutionSequence() {
+  const stringLog = logArr.join(' / ');
+  console.log(`log: ${stringLog}`);
+}
+
+// 테스트 하기!
+function testCase() {
+  logArr = [];
+  getArea('circle', 10);
+  getArea('circle', 1, 10);
+  getArea('rect', 10, 20);
+  getArea('trapezoid', 10, 20, 50);
+  printExecutionSequence();
+}
+testCase();
 
 // node.js 로 입력 값 받아서 결과 도출하기
 // 참고 링크 https://nscworld.net/2020/12/28/node-js%EC%97%90%EC%84%9C-%EC%9E%85%EB%A0%A5%EB%B0%9B%EB%8A%94-%EB%B0%A9%EB%B2%95/
@@ -47,95 +137,18 @@ const std = readline.createInterface({
 
 std
   .on('line', (line) => {
-    input = line.split('$').map((shape) => shape);
-    // 문제 1. 입력 값에 상관 없이 getArea() 4번 실행
-    // 문제 2. ...size를 못받아옴
+    // 1. 사용자 입력 형태 : circle,10/circle,1,10/rect,10,20/trapezoid,10,20,50
+    // 2. ['shape,number,number', 'shape,number,number'] 형태로 분리해 배열 만듬
+    // 3. 반복문으로 getArea(shape, number, number) 호출
 
-    // 작업 필요
-    // 1. shape 말고 다른 인자들 받아오기
-    // 2. 먼저 (shape + ...size)를 묶어서 배열에 하나씩 넣고
-    // 3. 배열 하나씩 차례대로 getArea()에 인자를 넣어서 실행
-    getArea(input[0], 10);
-    getArea(input[1], 1, 10);
-    getArea(input[2], 10, 20);
-    getArea(input[3], 10, 20, 50);
+    logArr = [];
+    userInputArr = line.split('/');
+    userInputArr.forEach((value) => {
+      const singleArr = value.split(',');
+      getArea(...singleArr);
+    });
     printExecutionSequence();
+
     std.close();
   })
   .on('close', () => process.exit());
-
-// 전역변수
-let logArr = []; //log 저장 배열
-
-// Get Area
-function getArea(shape, ...size) {
-  switch (shape) {
-    case 'rect':
-      getRect(...size);
-      break;
-    case 'trapezoid':
-      getTrapezoid(...size);
-      break;
-    case 'circle':
-      getCircle(...size);
-      break;
-    default:
-      console.log('도형을 정확하게 입력해주세요.');
-      return false;
-  }
-  saveExecutionSequence(shape, result);
-  return result;
-}
-
-// Rect
-function getRect(width, height) {
-  result = width * height;
-  return result;
-}
-
-// Trapezoid
-function getTrapezoid(shortBase, longBase, height) {
-  result = ((shortBase + longBase) * height) / 2;
-  return result;
-}
-
-// Single Circle or Sum Circles
-function getCircle(radius1, radius2 = 'empty') {
-  const PI = Math.PI;
-
-  if (radius2 === 'empty') {
-    // single
-    result = radius1 * radius1 * PI;
-  } else {
-    // multiple
-    result = 0;
-    for (let i = radius1; i < radius2 + 1; i++) {
-      result += i * i * PI;
-    }
-  }
-  return result;
-}
-
-// Save Log
-function saveExecutionSequence(shape, result) {
-  logArr.push(`${shape}, ${result}`);
-}
-
-// Print Log
-function printExecutionSequence() {
-  const printLog = logArr.join(' / ');
-  console.log(printLog);
-}
-
-// 테스트 하기
-function testCase() {
-  logArr = [];
-  getArea('circle', 0);
-  getArea('circle', 1, 10);
-  getArea('rect', 10, 20);
-  getArea('trapezoid', 10, 20, 50);
-  printExecutionSequence();
-  console.log(logArr);
-  return logArr;
-}
-//testCase();
