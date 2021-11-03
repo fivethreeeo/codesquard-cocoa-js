d7708a5ab05ab9a3a759f00df7e4d620f59e64b5
Sangbeom Heo <mansaout@gmail.com>
2021-11-03 17:59:35 +0900

    함수 분리

--- mission_02_notation.js
+++ mission_02_notation.js
@@ -29,53 +29,69 @@ gildongTurn -> 길동이 순서(숫자)
 
 3.
 notationArray의 숫자들을 1자리 씩 쪼개서
-AllAnswerArray 배열에 추가
+allAnswer 배열에 추가
 
 4.
 gildong의 차례 구하기
-gildong의 차례의 대답을 gildongAnswerArray 배열에 추가
+gildong의 차례의 대답을 gildongAnswer 배열에 추가
 */
 
 'use-strict';
 
+// 진수 변환기 + 길동이 차례 대답 찾기
 function playNotation(notation, count, playerNumber, gildongTurn) {
-  // 예외처리
   if (notation > 1 && notation < 17 === false) {
     console.log('2~16의 숫자를 입력하세요.');
     return;
   }
 
-  // 1
   const totalCount = count * playerNumber;
-
-  // 2
-  const notationArray = [];
-  for (let i = 0; i < totalCount; i++) {
-    const num = i.toString(notation);
-    notationArray.push(num);
-  }
-
-  // 3
-  const AllAnswerArray = Array.from(notationArray.join(''));
+  const notationArray = doNotation(totalCount, notation); // 2
+  const allAnswer = makeAnswer(notationArray); // 3
 
   // 4
-  const gildongAnswerArray = [];
-  const gildongFirstIndex = gildongTurn - 1;
-  const gap = playerNumber;
-
-  for (let i = 0; i < AllAnswerArray.length; i++) {
-    const gildongIndex = gildongFirstIndex + i * gap;
-    const value = AllAnswerArray[gildongIndex];
-    value && gildongAnswerArray.push(value);
-  }
+  const gildongAnswer = findPlayerAnswer(allAnswer, playerNumber, gildongTurn);
 
   // print
   console.log(
     `변환할 진수: ${notation} / 숫자 개수: ${totalCount} / 참가자 수: ${playerNumber} / 길동's 순서: ${gildongTurn}`
   );
   console.log(`8진수 변환 : ${notationArray}`);
-  console.log(`1자리씩 대답: ${AllAnswerArray}`);
-  console.log(`길동이 차례 대답: ${gildongAnswerArray}`);
+  console.log(`1자리씩 대답: ${allAnswer}`);
+  console.log(`길동이 차례 대답: ${gildongAnswer}`);
 }
 
-playNotation(20, 4, 4, 3);
+// 0~count 숫자를 notation진수로 변환한 배열을 만듬
+function doNotation(count, notation) {
+  const result = [];
+  for (let i = 0; i < count; i++) {
+    const num = i.toString(notation);
+    result.push(num);
+  }
+  return result;
+}
+
+// 받은 내용을 하나의 문자열로 만들고 그것을 다시 1자리씩 분리한 배열을 만듬
+function makeAnswer(array) {
+  return Array.from(array.join(''));
+}
+
+// 전체 대답, 참가자 수, 특정 참가자의 차례를 받아서 특정 참가자의 대답을 찾아줌
+function findPlayerAnswer(allAnswer, playerNumber, playerTurn) {
+  const playerAnswer = [];
+  const firstIndex = playerTurn - 1;
+  const gap = playerNumber;
+
+  for (let i = 0; i < allAnswer.length; i++) {
+    const playerIndex = firstIndex + i * gap;
+    const value = allAnswer[playerIndex];
+    value && playerAnswer.push(value);
+  }
+  return playerAnswer;
+}
+
+// testCase
+function testCase() {
+  playNotation(8, 4, 4, 3);
+}
+testCase();
