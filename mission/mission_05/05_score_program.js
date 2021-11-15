2564f1c8faf3f3a8705c7cd9feec46ec7e235748
Sangbeom Heo <mansaout@gmail.com>
2021-11-15 10:29:31 +0900

    add mission_05

---
+++ mission/mission_05/05_score_program.js
@@ -0,0 +1,42 @@
+'use strict';
+
+/*
+
+  ** 문제 1 **
+
+  - [] 평균
+  - [] 표쥰편차
+  - [] 70-80점 사이의 비율
+
+  - [] 객체 단위 프로그램
+
+*/
+
+const scores =
+  'math, 89.23, 82.03, 71.56, 78.82, 85.05, 84.44, 67.53, 71.7, 77.97, 73.77, 84.25, 67.01, 73.78, 64.19, 89.89, 90.32, 73.21, 75.35, 83.22, 74.01';
+
+// 1. readline으로 scores 입력 받음
+
+// nodejs - realline
+
+// const readline = require('readline');
+// const std = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+// });
+
+// std
+//   .on('line', (line) => {
+//     std.close();
+//   })
+//   .on('close', () => process.exit());
+
+// 2. scores 를 과목:점수 형태의 객체로 할당.
+
+// 3. getMean() -> 평균 구하기
+function getMean() {}
+
+// 4. getStandardDeviation() -> 표준편차 구하기
+function getStandardDeviation() {}
+
+// 5. 프로그램을 객체로 전환
