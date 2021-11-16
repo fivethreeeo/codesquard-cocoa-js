fa2f949091374b60db2cfbbcbf7bfc54a10d3a85
Sangbeom Heo <mansaout@gmail.com>
2021-11-17 00:39:43 +0900

    기능구현 : nodejs 로 input받아서 결과출력하기

--- mission/mission_05/05_score_program.js
+++ mission/mission_05/05_score_program.js
@@ -9,7 +9,7 @@
     - [o] 객체 단위 프로그램
     - [x] 70-80점 사이의 비율 구하기
 
-    - [x] 비동기 사용자 입력
+    - [o] 비동기 사용자 입력
     - [o] 과목별로 출력
     - [o] 퀵소트로 점수 정렬
       - 05_quick_sort.js에 퀵소트 구현
@@ -22,71 +22,25 @@
     표쥰편차 = 평균제곱의 제곱근
 */
 
-// 사용자 입력 값
-const userInput1 =
-  'math, 89.23, 82.03, 71.56, 78.82, 85.05, 84.44, 67.53, 71.70, 77.97, 73.77, 84.25, 67.01, 73.78, 64.19, 89.89, 90.32, 73.21, 75.35, 83.22, 74.01';
-const userInput2 =
-  'physics, 80, 90, 60, 70, 50, 50, 80, 70, 70, 70, 80, 60, 70, 60, 80, 90, 70, 70, 80, 70';
-
-// **** 점수 계산 프로그램 ****
 class ScoreProgram {
   constructor() {
     this.scoreSet = {};
   }
 
-  // 사용자 입력 추가
-  enterScoreNode() {
-    return new Promise((resolve) => {
-      const readline = require('readline');
-      const rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout,
-      });
-
-      // 입력 값 저장
-      const inputSet = [];
-
-      console.log('과목명, 점수를 입력해주세요. -> math, 80, 90, 70, ...');
-      console.log('입력을 종료하시려면 "quit"를 입력해주세요.');
-
-      rl.setPrompt('> ');
-      rl.prompt();
-      rl.on('line', function (line) {
-        switch (line) {
-          case 'quit':
-            console.log('입력을 완료했습니다.');
-            console.log(inputSet);
-            rl.close();
-          default:
-            console.log('입력 내용: ', line);
-            inputSet.push(`${line}`);
-            console.log(inputSet);
-            rl.prompt();
-        }
-      });
-      rl.on('close', function () {
-        process.exit();
-      });
-
-      resolve(inputSet);
+  enterScore(userInput) {
+    userInput.forEach((input) => {
+      const split = input.split(',');
+      const subjectName = split.splice(0, 1);
+      this.scoreSet[subjectName] = split.map((score) => Number(score));
     });
   }
 
-  // 기존 데이터 추가
-  enterScore(input) {
-    const split = input.split(', ');
-    const subjectName = split.splice(0, 1);
-    this.scoreSet[subjectName] = split.map((score) => Number(score));
-  }
-
-  // 점수 개수
   getCount(subject) {
     const count = this.scoreSet[subject].length;
 
     return count;
   }
 
-  // 평균
   getMean(subject) {
     const count = this.getCount(subject);
     const totalScore = this.scoreSet[subject].reduce((sum, curr) => sum + curr);
@@ -95,7 +49,6 @@ class ScoreProgram {
     return mean;
   }
 
-  // 편차
   getDeviation(subject) {
     const mean = this.getMean(subject);
     const scores = this.scoreSet[subject];
@@ -104,17 +57,16 @@ class ScoreProgram {
     return deviation;
   }
 
-  // 분산(평균제곱)
   getDispersion(subject) {
     const count = this.getCount(subject);
     const deviation = this.getDeviation(subject);
     const disperson =
-      deviation.reduce((sum, deviation) => deviation ** 2 + sum, 0) / count;
+      deviation.reduce((sum, deviation) => Math.pow(deviation, 2) + sum, 0) /
+      count;
 
     return disperson;
   }
 
-  // 표준편차
   getStandardDeviation(subject) {
     const disperson = this.getDispersion(subject);
     const standardDeviation = Math.sqrt(disperson);
@@ -122,7 +74,6 @@ class ScoreProgram {
     return standardDeviation;
   }
 
-  // 오름차순 정렬
   sortScore(subject) {
     const result = this.scoreSet[subject].sort((a, b) => a - b);
 
@@ -130,19 +81,79 @@ class ScoreProgram {
   }
 }
 
-function gogo() {
-  const goScore = new ScoreProgram();
-  goScore.enterScoreNode();
-  goScore.enterScore(userInput1);
-  goScore.enterScore(userInput2);
-  console.log(goScore.scoreSet);
-  console.log(`math 평균: ${goScore.getMean('math')}`);
-  console.log(`math 표준편차: ${goScore.getStandardDeviation('math')}`);
-  console.log(`physics 평균: ${goScore.getMean('physics')}`);
-  console.log(`physics 표준편차: ${goScore.getStandardDeviation('physics')}`);
-  console.log(`math 정렬: ${goScore.sortScore('math')}`);
-  console.log(`physics 정렬: ${goScore.sortScore('physics')}`);
-  console.log(goScore.scoreSet);
+function testUserData() {
+  const userData = [
+    'math,89.23,82.03,71.56,78.82,85.05,84.44,67.53,71.70,77.97,73.77,84.25,67.01,73.78,64.19,89.89,90.32,73.21,75.35,83.22,74.01',
+    'physics,80,90,60,70,50,50,80,70,70,70,80,60,70,60,80,90,70,70,80,70',
+  ];
+
+  const score = new ScoreProgram();
+  console.log(`
+testUserData-----------------------------------------------
+`);
+  score.enterScore(userData);
+  console.log(`입력된 과목      : ${Object.keys(score.scoreSet)}`);
+  console.log(`math 평균        : ${score.getMean('math')}`);
+  console.log(`math 표준편차    : ${score.getStandardDeviation('math')}`);
+  console.log(`math 정렬        : ${score.sortScore('math')}`);
+  console.log(`physics 평균     : ${score.getMean('physics')}`);
+  console.log(`physics 표준편차 : ${score.getStandardDeviation('physics')}`);
+  console.log(`physics 정렬     : ${score.sortScore('physics')}`);
+}
+//testUserData();
+
+function testNodeInput() {
+  const score = new ScoreProgram();
+
+  const doReadline = (printResult) => {
+    const readline = require('readline');
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
+
+    console.log(`
+testNodeInput--------입력받기----------------------------------
+`);
+    console.log('과목명, 점수를 입력해주세요. 예시) math, 80, 90, 70, ...');
+    console.log('입력을 종료하시려면 "quit"를 입력해주세요.');
+
+    const lines = [];
+
+    rl.on('line', function (line) {
+      switch (line) {
+        case 'quit':
+          console.log('입력을 완료했습니다.');
+          rl.close();
+        default:
+          lines.push(`${line}`);
+      }
+    });
+
+    rl.on('close', function () {
+      printResult(lines);
+      process.exit();
+    });
+  };
+
+  function calculateScore(lines) {
+    score.enterScore(lines);
+    console.log(`
+testNodeInput--------결과출력----------------------------------
+`);
+    console.log(`입력된 과목      : ${Object.keys(score.scoreSet)}`);
+    console.log(`math 평균        : ${score.getMean('math')}`);
+    console.log(`math 표준편차    : ${score.getStandardDeviation('math')}`);
+    console.log(`math 정렬        : ${score.sortScore('math')}`);
+    console.log(`physics 평균     : ${score.getMean('physics')}`);
+    console.log(`physics 표준편차 : ${score.getStandardDeviation('physics')}`);
+    console.log(`physics 정렬     : ${score.sortScore('physics')}`);
+  }
+
+  doReadline(calculateScore);
 }
 
-gogo();
+testNodeInput();
+// 두 줄 입력하기
+// line1: math,89.23,82.03,71.56,78.82,85.05,84.44,67.53,71.70,77.97,73.77,84.25,67.01,73.78,64.19,89.89,90.32,73.21,75.35,83.22,74.01
+// line2: physics,80,90,60,70,50,50,80,70,70,70,80,60,70,60,80,90,70,70,80,70
