a77dc823d87ca803431302529b287a9db4624af5
Sangbeom Heo <mansaout@gmail.com>
2021-11-25 13:50:32 +0900

    데이터 수정 : 작성된 todoStorage의 content 가데이터들의 문자열 변경

--- mission/mission_06/todo_list_ver_week/src/js/model.js
+++ mission/mission_06/todo_list_ver_week/src/js/model.js
@@ -13,19 +13,22 @@ export default class TodoModel {
     this.doneCount = 0;
     this.todoStorage = {
       '2021-11-24': {
-        1637772390084: { status: 'notYet', content: '1' },
-        1637772390522: { status: 'done', content: '2' },
-        1637772391715: { status: 'done', content: '3' },
+        1637772390084: { status: 'notYet', content: '비동기 학습하기' },
+        1637772390522: { status: 'done', content: '디버깅 학습하기' },
+        1637772391715: {
+          status: 'done',
+          content: 'JavaScript map APIs 학습하기',
+        },
       },
       '2021-11-25': {
-        1637772027295: { status: 'notYet', content: '111111' },
-        1637772027486: { status: 'done', content: '222222' },
-        1637772027655: { status: 'notYet', content: '333333' },
+        1637772027295: { status: 'notYet', content: '마스터즈 가고 싶다..' },
+        1637772027486: { status: 'done', content: '정규 표현식 학습' },
+        1637772027655: { status: 'notYet', content: '클래스 상속 학습' },
       },
       '2021-11-26': {
-        1637772394423: { status: 'notYet', content: '111' },
-        1637772394592: { status: 'done', content: '222' },
-        1637772394761: { status: 'notYet', content: '333' },
+        1637772394423: { status: 'notYet', content: 'mvc 학습' },
+        1637772394592: { status: 'done', content: '클로저 학습하기' },
+        1637772394761: { status: 'notYet', content: '마스터즈 가기..' },
       },
     };
   }
